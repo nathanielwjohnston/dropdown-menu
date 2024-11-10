3b96a743b44584feec1701195b530b2fd087c3b9
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-11-10 20:22:10 +0000

    Close one dropdown when another opens

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ body.addEventListener("click", e => {
       if (menu.classList.contains("closed")) {
         menu.classList.remove("closed");
         menu.classList.add("open");
+        menus.forEach(selectedMenu => {
+          if (selectedMenu.classList.contains("open") && selectedMenu !== menu) {
+            selectedMenu.classList.remove("open");
+            selectedMenu.classList.add("closed");
+          }
+        })
       } else if (menu.classList.contains("open")) {
         menu.classList.remove("open");
         menu.classList.add("closed");
